60e53aeeed03c3f6ac4f9bfa8fb445462fd3b4e2
Georgi Georgiev <nlyakov@influxtechnology.com>
2022-01-31 13:39:28 +0200

    Python scripts added

---
+++ cansend.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+import os
+import re
+import time
+
+while True:
+	with open('./can', 'a') as f:
+		timestamp = 12345
+		channel = 0
+		ident = 298
+		dlc = 5
+
+		data = [12, 13, 14, 15, 16]
+		
+		line = "(" + '{}'.format(timestamp) + ") can" + '{}'.format(channel) + " " + '{:x}'.format(ident) + " [" + '{}'.format(dlc) + "]"
+
+		for i in range(dlc):
+				line = line + " " + '{:02x}'.format(data[i])
+
+		f.write(line + "\n");
+		time.sleep(1);
+
